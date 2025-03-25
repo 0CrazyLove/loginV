fa406f92df83761419c21a6661f31fb7fb00891c
0CrazyLove <axelvasquez582@gmail.com>
2025-03-25 02:06:59 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const tocPassword= document.getElementById("tocarPassowrd");
 tocPassword.addEventListener("click", function(){
     if(inputPassword.type === "password"){
         inputPassword.type="text";
-        tocPassword.src="/login/img/vista.png";
+        tocPassword.src="/loginMovil/ojo.png";
        
     } else {
         inputPassword.type="password";
-        tocPassword.src="/login/img/ojo.png";
+        tocPassword.src="/loginMovil/ojo.png";
     }
 
 
